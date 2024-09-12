b09fd9decb6058facd5ddd48475b2fa6204de3ec
Anju6770 <anjalighalleyrai@gmail.com>
2024-09-12 10:14:12 +0600

    commit

--- lib/pages/wall_details.dart
+++ lib/pages/wall_details.dart
@@ -12,6 +12,7 @@ class WallDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
